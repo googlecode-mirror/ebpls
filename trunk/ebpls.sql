21fd8f8f255246c22e241c88993ebb6714a6ef2c
ron.crabtree <ron.crabtree@44719721-7e4a-0410-be69-196204798cfa>
2008-02-29 12:00:00 +0000

    included ebpls_payment_details table in ebpls.sql

--- trunk/ebpls.sql
+++ trunk/ebpls.sql
@@ -3589,4 +3589,25 @@ CREATE TABLE vehicle_transfer (
 -- 
 -- Dumping data for table 'vehicle_transfer'
 -- 
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table 'ebpls_payment_details'
+-- 
+
+DROP TABLE IF EXISTS ebpls_payment_details;
+CREATE TABLE ebpls_payment_details (
+  payment_details_id int(21) NOT NULL auto_increment,
+  payment_details varchar(255) NOT NULL default '',
+  or_no varchar(255) NOT NULL default '',
+  owner_id int(21) NOT NULL default '0',
+  business_id int(21) NOT NULL default '0',
+  natureid int(21) NOT NULL default '0',
+  tfoid int(21) NOT NULL default '0',
+  amount decimal(15,2) NOT NULL default '0.00',
+  payment_part int(1) NOT NULL default '0',
+  date_create datetime NOT NULL default '0000-00-00 00:00:00',
+  created_by varchar(255) NOT NULL default '',
+  PRIMARY KEY  (payment_details_id)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
