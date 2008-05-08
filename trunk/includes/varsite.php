498e9aa3680b50a7a24c1a50c3f819befe0855cc
ron.crabtree <ron.crabtree@44719721-7e4a-0410-be69-196204798cfa>
2008-05-08 14:52:55 +0000

    Creates sql folder and moves SQL files there. **OOPS This modifies setup/settings.php and deletes includes/varsite.php. Please recover these from Trash.

--- trunk/includes/varsite.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("eBPLS_APP_URL", "192.168.1.15/");	// do not include filenames
-?>
--- trunk/setup/setting.php
+++ trunk/setup/setting.php
@@ -2,8 +2,8 @@
 
 $thIntPassLen = 20;
 $thIntPassRetLimit = 3;
-$thIntCookieExp = 3600;
-$thIntPageLimit = 10;
+$thIntCookieExp = 5184000;
+$thIntPageLimit = 100;
 $thThemeColor1 = "#DDE3EB";
 $thThemeColor2 = "#6FCDF9";
 $thThemeColor3 = "#FFFFFF";
@@ -15,7 +15,7 @@ $thIntCronIntervalExp = 28800;
 $thDbHost = "localhost";
 $thDbUser = "root";
 $thDbUKey = "elguebpls";
-$thDbName = "ebplssample";
+$thDbName = "ebpls";
 $thDbLink = 0;
 $DBprefix = "ebpls_buss";
 
